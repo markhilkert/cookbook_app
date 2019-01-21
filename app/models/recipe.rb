73391322e7ec5f9c388bd4c463fa40230414b207
markhilkert <mark.hilkert@gmail.com>
2019-01-21 10:34:27 -0600

    added model methods to clean up recipe display

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,37 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    ingredients.split(", ")
+  end
+
+  def directions_list
+    directions.split(", ")
+  end
+
+  def friendly_created_at
+    created_at.strftime("%b %d, %Y")
+  end
+
+  def friendly_updated_at
+    updated_at.strftime("%b %d, %Y")
+  end
+
+  def friendly_prep_time
+    # input: integer, stored in prep_time attribute, with reader method. Ex: 125
+    # output: string - "2 Hours, 5 minutes."
+
+    # Find hours within prep_time, by dividing by 60
+    # Find minutes within prep_time, by finding the remainder
+    # Build a message, using the hours and minutes
+      # Only build hours part of the message if there are more than 0 hours
+
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    time_message = ""
+
+      time_message += "#{hours} #{'Hour'.pluralize(hours)}" if hours > 0
+      time_message += ", " if hours > 0 && minutes > 0 
+      time_message += "#{minutes} #{"Minute".pluralize(minutes)}" if minutes > 0
+
+    time_message
+  end
 end
--- app/views/api/recipes/index.json.jbuilder
+++ app/views/api/recipes/index.json.jbuilder
@@ -2,4 +2,9 @@ json.array! @recipes.each do |recipe|
   json.id recipe.id
   json.title recipe.title
   json.chef recipe.chef
+  json.ingredients recipe.ingredients_list
+  json.directions recipe.directions_list
+  json.prep_time recipe.friendly_prep_time
+  json.created_at recipe.friendly_created_at
+  json.updated_at recipe.friendly_updated_at
 end
